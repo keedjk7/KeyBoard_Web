dfae3c48bf476bb4293433a6cb47ad2f08723c91
Your Name <you@example.com>
2022-03-28 14:41:57 +0700

    login and register

---
+++ models/user.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    name: String,
+    username: {
+        type: String,
+        unique: true
+    },
+    password: {
+        type: String
+    }
+});
+
+const UserModel = mongoose.model('User', userSchema);
+
+module.exports = UserModel;
---
+++ routes/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+
+const isLoggedIn = (req, res, next) => {
+  if (!req.session.user) {
+    res.redirect('/login');
+  }
+  next();
+};
+
+router.get('/', isLoggedIn, function (req, res, next) {
+  res.render('index', { title: 'Keyboard Web' , user: req.session.user});
+});
+
+router.get('/register', (req, res) => {
+  res.render('register');
+});
+
+router.get('/login', (req, res) => {
+  res.render('login');
+});
+
+module.exports = router;
